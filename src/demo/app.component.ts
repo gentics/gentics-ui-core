a1260a9262c4e17a4b96f842e6d30b99ba366204
Michael Bromley <michael@michaelbromley.co.uk>
2016-02-15 15:40:27 +0100

    Create TopBar component

--- src/demo/app.component.ts
+++ src/demo/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from 'angular2/core';
-import {SearchBar, ListPane, ContentPane, SplitViewContainer} from '../index';
+import {TopBar, SearchBar, ListPane, ContentPane, SplitViewContainer} from '../index';
 
 @Component({
     selector: 'app',
     template: require('./app.tpl.html'),
-    directives: [SearchBar, ListPane, ContentPane, SplitViewContainer]
+    directives: [TopBar, SearchBar, ListPane, ContentPane, SplitViewContainer],
+    styles: [require('./app.scss').toString()]
 })
 export class App {
 
