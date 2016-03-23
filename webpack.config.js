7be6699e978696667a1e263e83fffa14a10295c5
Michael Bromley <apagtxbromley@apa.at>
2016-03-23 13:47:22 +0100

    Remove unused TS options from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -30,20 +30,6 @@ module.exports = {
             {
                 test: /\.ts$/,
                 loader: 'ts?transpileOnly=true',
-                query: {
-                    ignoreDiagnostics: [
-
-                        // There are several TS compiler errors that seem to be related to the way Angular2 incorporates
-                        // its own ambient type definition files. The following list squashes those errors, but should be
-                        // reviewed and eventually removed. See https://github.com/angular/angular/issues/5807
-                        2403, // 2403 -> Subsequent variable declarations
-                        2300, // 2300 -> Duplicate identifier
-                        2374, // 2374 -> Duplicate number index signature
-                        2375, // 2375 -> Duplicate string index signature
-                        2420, // 2420 -> Class x incorrectly implements interface Ix
-                        2502  // 2502 -> 'X' is referenced directly or indirectly in its own type annotation.
-                    ]
-                },
                 compilerOptions: {
                     noEmit: false,
                     noEmitOnError: false
