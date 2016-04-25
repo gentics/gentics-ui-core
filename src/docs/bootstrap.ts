7cb5b3d5f12819a6766316805cebe11d0d1daaa1
Michael Bromley <apagtxbromley@apa.at>
2016-04-25 15:19:21 +0200

    Various styling tweaks to docs app

--- src/docs/bootstrap.ts
+++ src/docs/bootstrap.ts
@@ -1,7 +1,7 @@
 require('zone.js');
 require('reflect-metadata');
 require('es6-shim');
-require('highlight.js/styles/zenburn.css');
+require('highlight.js/styles/atelier-estuary-light.css');
 
 import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
