5c94d130a320eb7d0788673b4e3059f67777c38f
Michael Bromley <apagtxbromley@apa.at>
2016-05-11 13:19:06 +0200

    Add show-on-hover class to ContentsListItem

--- src/docs/pages/contents-list-item-demo/contents-list-item-demo.ts
+++ src/docs/pages/contents-list-item-demo/contents-list-item-demo.ts
@@ -1,15 +1,15 @@
 import {Component} from '@angular/core';
-import {GTX_FORM_DIRECTIVES, ContentsListItem} from '../../../index';
+import {GTX_FORM_DIRECTIVES, ContentsListItem, Button} from '../../../index';
 import {Autodocs, DemoBlock, HighlightedCode} from '../../components';
 
 @Component({
     template: require('./contents-list-item-demo.tpl.html'),
-    directives: [GTX_FORM_DIRECTIVES, ContentsListItem, Autodocs, DemoBlock, HighlightedCode]
+    directives: [GTX_FORM_DIRECTIVES, Button, ContentsListItem, Autodocs, DemoBlock, HighlightedCode]
 })
 export class ContentsListItemDemo {
 
     componentSource: string = require('!!raw!../../../components/contents-list-item/contents-list-item.component.ts');
-    
+
     listItems: string[] = [
         'foo',
         'bar',
