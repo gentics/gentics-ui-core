bc0d3c756aec110e0f2e5adf6624916d68914c8e
Michael Bromley <apagtxbromley@apa.at>
2016-05-11 15:01:10 +0200

    Update lib

--- dist/components/contents-list-item/contents-list-item.component.js
+++ dist/components/contents-list-item/contents-list-item.component.js
@@ -16,6 +16,7 @@ var core_1 = require('@angular/core');
  *
  * * `.item-avatar`: The content of this element will be styled in a circular container.
  * * `.item-primary`: The primary content of the item, which will take up all the remaining space via `flex: 1`.
+ * * `.show-on-hover`: Any element with this class will appear faded out until the user hovers the list item.
  *
  *
  * ```html
@@ -25,8 +26,8 @@ var core_1 = require('@angular/core');
  *     <!-- this will stretch to use all available space -->
  *     <div class="item-primary"><a [routerLink]="[item.route]">{{ item.title }}</a></div>
  *     <!-- these will use remaining space to the right -->
- *     <i class="material-icons">edit</i>
- *     <i class="material-icons">star</i>
+ *     <i class="material-icons show-on-hover">edit</i>
+ *     <i class="material-icons show-on-hover">star</i>
  * </gtx-contents-list-item>
  * ```
  */
