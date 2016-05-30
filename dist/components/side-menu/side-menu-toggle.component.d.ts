36eba7701fc557cf4453c570340cc46b21f85cec
Leon Adler <leonadler@gentics.com>
2016-05-30 20:37:11 +0200

    Exclude dist folder from repo, fix typings

--- dist/components/side-menu/side-menu-toggle.component.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * An animated CSS hamburger menu icon, used by the SideMenu component to trigger the
- * "toggle" event. Only used internally by the SideMenu component.
- */
-export declare class SideMenuToggleButton {
-    active: boolean;
-}
--- dist/form-directives.d.ts
+++
@@ -1,6 +0,0 @@
-import { Type } from '@angular/core';
-/**
- * This file is here to group all the form components and their value accessors into a single
- * variable, to simplify their consumption.
- */
-export declare const GTX_FORM_DIRECTIVES: Type[];
