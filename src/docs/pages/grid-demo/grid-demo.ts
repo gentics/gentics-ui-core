901d410221e09a959ff4478ea43ca732bc04071b
Leon Adler <leonadler@gentics.com>
2016-06-22 17:09:44 +0200

    Fix webpack image URL errors in grid demo html

--- src/docs/pages/grid-demo/grid-demo.ts
+++ src/docs/pages/grid-demo/grid-demo.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {HighlightedCode} from '../../components';
 
 @Component({
-    template: require('./grid-demo.tpl.html'),
+    template: require('!!raw!./grid-demo.tpl.html'),
     directives: [HighlightedCode]
 })
 export class GridDemo {}
