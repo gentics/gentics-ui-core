8328b4deddbda9b16d7e3f4f5825bc90947a2dc8
Leon Adler <leonadler@gentics.com>
2017-02-06 16:04:04 +0100

    Docs typo / styling fixes

--- src/components/overlay-host/overlay-host.component.ts
+++ src/components/overlay-host/overlay-host.component.ts
@@ -5,7 +5,7 @@ import {OverlayHostService} from './overlay-host.service';
  * The OverlayHost is required to display any kind of overlay component such as a modal or
  * [toast notification](#/notification). It represents the location in the DOM into which new overlays
  * will be loaded. As such it should be at or close to the root of your app (i.e. a direct
- * child of you root App component).
+ * child of your App's root component).
  *
  * There are no attributes to configure nor events to listen to - all interaction with this
  * component happens via the `OverlayHostService`, which allows other components to grab a 
