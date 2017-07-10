5865fa12b05d7dd76b2ab33ab557333cf13cf71d
Michael Bromley <m.bromley@gentics.com>
2017-07-10 17:17:58 +0200

    Fix broken examples in docs

--- src/docs/pages/tabs-demo/tabs-demo.ts
+++ src/docs/pages/tabs-demo/tabs-demo.ts
@@ -5,5 +5,5 @@ import {Component} from '@angular/core';
 })
 export class TabsDemo {
     componentSource: string = require('!!raw-loader!../../../components/tabs/tabs.component');
-    activeTab: string;
+    activeTab: string = 'tab1';
 }
