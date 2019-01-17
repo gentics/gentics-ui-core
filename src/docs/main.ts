b0c1a5e6215927f51939519dde5df30062626b33
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2019-01-17 16:05:15 +0100

    Add and import angular-cli PROD/DEBUG environment definitions

--- src/docs/main.ts
+++ src/docs/main.ts
@@ -3,9 +3,9 @@ require('highlight.js/styles/atelier-estuary-light.css');
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {DocsModule} from './app.module';
+import {environment} from '../../projects/docs/environments/environment';
 
-declare const PROD: boolean;
-if (PROD) {
+if (environment.production) {
     enableProdMode();
 }
 
