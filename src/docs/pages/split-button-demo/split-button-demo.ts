cba90da2b034b5af4449aa35f486f146031d37b2
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2019-04-11 17:05:07 +0200

    Add split button demo

--- src/docs/pages/split-button-demo/split-button-demo.ts
+++ src/docs/pages/split-button-demo/split-button-demo.ts
@@ -2,13 +2,20 @@ import {Component} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
-    templateUrl: './split-button-demo.tpl.html',
-    styles: [`gtx-button { margin-bottom: 10px; }`]
+    selector: 'split-button-demo',
+    templateUrl: './split-button-demo.tpl.html'
 })
 export class SplitButtonDemo {
     componentSource: string = require('!!raw-loader!../../../components/split-button/split-button.component.ts');
+
+    saveResult: string;
+
+    secondaryActions: string[] = [];
+    actionClicked: number;
+
     buttonIsDisabled: boolean = false;
     clickCount: number = 0;
+
     formResult: any;
     demoForm = new FormGroup({
         firstName: new FormControl('John'),
@@ -18,4 +25,29 @@ export class SplitButtonDemo {
     incrementCounter(): void {
         ++this.clickCount;
     }
+
+    save(): void {
+        this.saveResult = 'Save clicked!';
+    }
+
+    saveAndPublish(): void {
+        this.saveResult = 'Save and Publish clicked!';
+    }
+
+    saveAndEmail(): void {
+        this.saveResult = 'Save and Send via E-Mail';
+    }
+
+    addSecondaryAction(): void {
+        this.secondaryActions.push('Secondary Action');
+    }
+
+    removeSecondaryAction(): void {
+        this.secondaryActions.pop();
+    }
+
+    onActionClick(actionId: number): void {
+        this.actionClicked = actionId;
+    }
+
 }
