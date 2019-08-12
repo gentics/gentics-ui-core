f404bbb179c60c72f3f53c6cea05b42600cfe62d
Iepikhin Oleksii <apagtxiepikhin@lan.apa.at>
2019-08-12 12:43:35 +0200

    Remove demo block for new implementation.

--- src/docs/pages/tabs-demo/tabs-demo.ts
+++ src/docs/pages/tabs-demo/tabs-demo.ts
@@ -1,18 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     templateUrl: './tabs-demo.tpl.html'
 })
-export class TabsDemo implements OnInit {
+export class TabsDemo {
     componentSource: string = require('!!raw-loader!../../../components/tabs/tabs.component.ts');
     activeTab: string = 'tab1';
     wrap: boolean = false;
-
-    canUseInbox: boolean = false;
-
-    ngOnInit(): void {
-        setTimeout(() => {
-            this.canUseInbox = true;
-        }, 3000);
-    }
 }
