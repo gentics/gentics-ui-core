32883f28643742c65010cc92b51ff48ab8f15db0
Philip Viktor Schulz-Klingauf <p.schulz-klingauf@gentics.com>
2022-05-09 16:06:27 +0200

    adapted polyfills

--- src/docs/polyfills.ts
+++ src/docs/polyfills.ts
@@ -22,33 +22,23 @@
  *  This also includes Android Emulators with older versions of Chrome and Google Search/Googlebot
  */
 
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/weak-map';
-import 'core-js/es/set';
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+// import 'core-js/es/symbol';
+// import 'core-js/es/object';
+// import 'core-js/es/function';
+// import 'core-js/es/parse-int';
+// import 'core-js/es/parse-float';
+// import 'core-js/es/number';
+// import 'core-js/es/math';
+// import 'core-js/es/string';
+// import 'core-js/es/date';
+// import 'core-js/es/array';
+// import 'core-js/es/regexp';
+// import 'core-js/es/map';
+// import 'core-js/es/weak-map';
+// import 'core-js/es/set';
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es/reflect';
-
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+// import 'core-js/es/reflect';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
